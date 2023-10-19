4bbe1c4c548f813c55e6d1665850869d4bd8d849
Ondrej Kolar <xkolar74@vutbr.cz>
2023-10-19 17:27:53 +0200

    cv04

---
+++ cv04-ADC/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: xkolar74
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+// Function prototypes
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t leds);
+
+#endif /* INC_SCT_H_ */
